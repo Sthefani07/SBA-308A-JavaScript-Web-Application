6134a26fbd195d551ff1edc9fc6ce2068975a929
Sthefani Denhof <sdenhof7@gmail.com>
2024-10-04 18:31:14 -0700

    Formatted HTML file.

---
+++ script.mjs
@@ -0,0 +1,5 @@
+// appID = 39912805
+// API Key: 80f32aa604cf4bdd9262044952631e68
+// https://spoonacular.com/food-api
+// https://api.spoonacular.com/food/products/search
+// https://api.spoonacular.com/recipes/1003464/nutritionWidget.json
