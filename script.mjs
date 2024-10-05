53b12ed5907bcad9f87c6e5eff5935f03740af4d
Sthefani Denhof <sdenhof7@gmail.com>
2024-10-05 00:12:57 -0700

    Formatting script.mjs.

--- script.mjs
+++ script.mjs
@@ -3,3 +3,25 @@
 // https://spoonacular.com/food-api
 // https://api.spoonacular.com/food/products/search
 // https://api.spoonacular.com/recipes/1003464/nutritionWidget.json
+
+const fruitForm = document.querySelector(".fruitForm");
+const fruitInput = docum.querySelector(".fruitInput");
+const card = document.querySelector("card");
+const apiKey = "80f32aa604cf4bdd9262044952631e68";
+
+fruitForm.addEventListener("submit", event => {
+
+});
+
+
+async function getFruitData(fruit){
+
+}
+
+function displayFruitInfo(data){
+
+}
+
+function errorDisplay(message){
+
+}
