5170fb14ec441b583db3f3d65be64b9a0049a31e
Sthefani Denhof <sdenhof7@gmail.com>
2024-10-05 12:54:45 -0700

    ErrorDisplay(message)

--- script.mjs
+++ script.mjs
@@ -1,21 +1,40 @@
-// appID = 39912805
-// API Key: 80f32aa604cf4bdd9262044952631e68
+//appID = 39912805
+//API Key: 80f32aa604cf4bdd9262044952631e68
 // https://spoonacular.com/food-api
 // https://api.spoonacular.com/food/products/search
 // https://api.spoonacular.com/recipes/1003464/nutritionWidget.json
 
 const fruitForm = document.querySelector(".fruitForm");
-const fruitInput = docum.querySelector(".fruitInput");
-const card = document.querySelector("card");
+const fruitInput = document.querySelector(".fruitInput");
+const card = document.querySelector(".card");
 const apiKey = "80f32aa604cf4bdd9262044952631e68";
 
-fruitForm.addEventListener("submit", event => {
+fruitForm.addEventListener("submit", async event => {
+    event.preventDefault(); //prevent the page to reload when click on the btn
+    
+    const fruit = fruitInput.value;
+
+    if(fruit){  //if there is fruit do this next ->>
+        try{
+            const fruitData = await getFruitData(fruit);
+            displayFruitInfo(fruitData);
+        }
+        catch(error){
+            console.error(error);
+            errorDisplay(error);
+        }
+    }
+    else{
+        errorDisplay("Please type your fruit");
+    }
 
 });
 
 
 async function getFruitData(fruit){
-
+    const apiUrl = `https://api.spoonacular.com/food/products/search`
+    const response = await fetch(apiUrl);
+    console.log(response)
 }
 
 function displayFruitInfo(data){
@@ -23,5 +42,11 @@ function displayFruitInfo(data){
 }
 
 function errorDisplay(message){
+    const displayError = document.createElement("p");
+    displayError.textContent = message;
+    displayError.classList.add("displayError");
 
+    card.textContent = "" ;
+    card.style.display = "flex";
+    card.appendChild(displayError);
 }
