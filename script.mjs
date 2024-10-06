4c41a6f3b3bfe670c6b0303d5aecbfdeca751f8d
Sthefani Denhof <sdenhof7@gmail.com>
2024-10-05 19:05:33 -0700

    API is working.

--- script.mjs
+++ script.mjs
@@ -7,41 +7,66 @@
 const fruitForm = document.querySelector(".fruitForm");
 const fruitInput = document.querySelector(".fruitInput");
 const card = document.querySelector(".card");
-const apiKey = "80f32aa604cf4bdd9262044952631e68";
+//const apiKey = "80f32aa604cf4bdd9262044952631e68";
 
 fruitForm.addEventListener("submit", async event => {
     event.preventDefault(); //prevent the page to reload when click on the btn
     
-    const fruit = fruitInput.value;
+    const fruit = fruitInput.value.trim();
 
-    if(fruit){  //if there is fruit do this next ->>
+    if(fruit)
+        {  //if there is fruit do this next ->>
         try{
             const fruitData = await getFruitData(fruit);
             displayFruitInfo(fruitData);
         }
-        catch(error){
+        catch(error)
+        {
             console.error(error);
-            errorDisplay(error);
+            errorDisplay("Error fetching fruit data: " + error.message);
         }
-    }
-    else{
+    }else
+    {
         errorDisplay("Please type your fruit");
     }
 
 });
 
 
-async function getFruitData(fruit){
-    const apiUrl = `https://api.spoonacular.com/food/products/search`
+async function getFruitData(fruit) {
+    const apiUrl = `https://api.spoonacular.com/food/ingredients/search?apiKey=80f32aa604cf4bdd9262044952631e68&query=${fruit}`;
     const response = await fetch(apiUrl);
-    console.log(response)
+
+    if (!response.ok) {
+        throw new Error("Could not fetch fruit data");
+    }
+
+    return await response.json();
 }
 
-function displayFruitInfo(data){
+
+getFruitData('banana')  //call function
+    .then(data => {
+        console.log(data);  // Print fruit
+    })
+    .catch(error => {
+        console.error(error);  // Handles and prints any errors
+    });
+
+
+
+async function displayFruitInfo(data)
+{
+
+    console.log(data)
 
 }
 
-function errorDisplay(message){
+
+
+
+function errorDisplay(message)
+{
     const displayError = document.createElement("p");
     displayError.textContent = message;
     displayError.classList.add("displayError");
