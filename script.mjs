21622fa067ec52441c38a04caf533a3889db8a22
Sthefani Denhof <sdenhof7@gmail.com>
2024-10-06 16:08:51 -0700

    Changed the whole plan, API did not work.

--- script.mjs
+++ script.mjs
@@ -4,74 +4,98 @@
 // https://api.spoonacular.com/food/products/search
 // https://api.spoonacular.com/recipes/1003464/nutritionWidget.json
 
-const fruitForm = document.querySelector(".fruitForm");
-const fruitInput = document.querySelector(".fruitInput");
+// "It didn’t work. I tried to create a nutrition table for fruits but had no success with any API out there."
+
+
+
+
+
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-//const apiKey = "80f32aa604cf4bdd9262044952631e68";
+const apiKey = "ba6f482929ef4cbaf04ebdb728809d58";
 
-fruitForm.addEventListener("submit", async event => {
-    event.preventDefault(); //prevent the page to reload when click on the btn
-    
-    const fruit = fruitInput.value.trim();
 
-    if(fruit)
-        {  //if there is fruit do this next ->>
+//adicionando o eventlistener para quando clicar no botao e o web interagir e responder
+weatherForm.addEventListener("submit", async event => {
+    event.preventDefault(); //prevent web to reset
+
+    const city = cityInput.value;
+    if(city){
         try{
-            const fruitData = await getFruitData(fruit);
-            displayFruitInfo(fruitData);
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData)
         }
-        catch(error)
-        {
-            console.error(error);
-            errorDisplay("Error fetching fruit data: " + error.message);
+         catch(error){
+           console.error(error);
+             displayError(error);
         }
-    }else
-    {
-        errorDisplay("Please type your fruit");
     }
-
+    else{
+        displayError("Please enter a city.")
+    }
 });
 
-
-async function getFruitData(fruit) {
-    const apiUrl = `https://api.spoonacular.com/food/ingredients/search?apiKey=80f32aa604cf4bdd9262044952631e68&query=${fruit}`;
+//
+async function getWeatherData(city){
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    
     const response = await fetch(apiUrl);
+    //console.log(response);
+     if(!response.ok){
+         throw new error("Could not fetch data");
 
-    if (!response.ok) {
-        throw new Error("Could not fetch fruit data");
     }
+     return await response.json();
+    
+ }
 
-    return await response.json();
+
+
+
+
+function displayWeatherInfo(data){
+    
+    const {name: city, 
+        main: {temp,humidity}, 
+        weather: [{description, id}]} = data;
+
+    card.textContent = "",
+    card.style.display = "flex"
+console.log(data)
+    const cityDisplay = document.createElement("h1")
+    const tempDisplay = document.createElement("p")
+    const humidityDisplay = document.createElement("p")
+    const descDisplay = document.createElement("p")
+
+    cityDisplay.textContent = city;
+    tempDisplay.textContent = `${((temp - 273.15) * (9/5) + 32).toFixed(1)} F`;
+    humidityDisplay.textContent = `humidity: ${humidity}%`;
+    descDisplay.textContent = description;
+
+
+    cityDisplay.classList.add("cityDisplay");
+    tempDisplay.classList.add("tempDisplay");
+    humidityDisplay.classList.add("humidityDisplay");
+    descDisplay.classList.add("descDisplay");
+
+
+    card.appendChild(cityDisplay);
+    card.appendChild(tempDisplay);
+    card.appendChild(humidityDisplay);
+    card.appendChild(descDisplay);
+    
 }
 
 
-getFruitData('banana')  //call function
-    .then(data => {
-        console.log(data);  // Print fruit
-    })
-    .catch(error => {
-        console.error(error);  // Handles and prints any errors
-    });
+//Error handler 
+function displayError(message){
+    const erroDisplay = document.createElement("p");
+    erroDisplay.textContent = message;
+    erroDisplay.classList.add("erroDisplay")
 
-
-
-async function displayFruitInfo(data)
-{
-
-    console.log(data)
-
-}
-
-
-
-
-function errorDisplay(message)
-{
-    const displayError = document.createElement("p");
-    displayError.textContent = message;
-    displayError.classList.add("displayError");
-
-    card.textContent = "" ;
+    card.textContent = "";
     card.style.display = "flex";
-    card.appendChild(displayError);
+    card.appendChild(erroDisplay);
+
 }
